f9d2277544afb9e37d7229aa16e38520053b79dc
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-03 08:42:38 +0530

    Bubble sort develop

--- src/Bubble_sort.java
+++ src/Bubble_sort.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 
-
 public class Bubble_sort {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		int i,j,k,l;
+
+		int i, j, k, l;
 		int a[] = new int[10];
 		int b[] = new int[10];
 		int c[] = new int[10];
 		System.out.println("enter the number to short");
-		for(i=0;i<=10;i++){
+		for (i = 0; i < 10; i++) {
 			a[i] = sc.nextInt();
 		}
-		//now the code for shorting the element
-		for(i=0;i<=10;i++){
-			if(a[i] > b[i]){
-				j = a[i];
-				a[i] = b[i];
-				b[i] = j;
+		// now the code for shorting the element
+		for (k = 0; k < 10; k++) {
+			for (i = 0; i < 10; i++) {
+				if (a[k] > b[i]) {
+					j = a[k];
+					a[k] = b[i];
+					b[i] = j;
+				}
 			}
-		System.out.println(b[i]);	
-		}	
+		}
+		for (k =0; k < 10; k++) {
+			System.out.println(b[k]);
+		}
 	}
-
 }
